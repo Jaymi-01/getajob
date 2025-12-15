015777704ff2518ad1f58dc89a76d90ac41bbd7e
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-12-15 13:49:51 +0100

    add Footer component for site-wide footer display

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="p-4 mt-8 bg-gray-100">
+      <div className="container mx-auto text-center text-gray-600">
+        <p>&copy; 2025 GetaJob. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
